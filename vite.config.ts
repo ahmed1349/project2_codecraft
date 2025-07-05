50d9f28af22e4e2dbc3c577fdeadaa9850f23292
ahmed1349 <brillianta134@gmail.com>
2025-07-05 17:42:01 +0300

    edit contact

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+
+  base: '/project2_codecraft/', // <- this is the fix!
   server: {
     host: "::",
     port: 8080,
